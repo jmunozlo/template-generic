0f4636fcfac26ca21f6605ec4690bb67d67d59b3
Jose Muñoz <jmunozlo.dev@gmail.com>
2024-07-24 12:27:21 -0400

    feat: version 0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Agendamiento</h1>
+      <h1>Agendamiento v0.0.1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
