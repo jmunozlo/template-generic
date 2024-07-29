003e16a8b68732a13a150ec8bbec7d826ca91745
Jose Muñoz <jmunozlo.dev@gmail.com>
2024-07-29 14:29:08 -0400

    feat: config design login

--- src/core/routers/router.tsx
+++ src/core/routers/router.tsx
@@ -1,17 +1,12 @@
 import { PrivateLayout } from '@/pages/layouts/private-layout'
-import { RootLayout } from '@/pages/layouts/root-layout'
 import { LoginPage } from '@/pages/LoginPage'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />,
+    element: <LoginPage />,
     children: [
-      {
-        index: true,
-        element: <LoginPage />,
-      },
       {
         path: 'about',
         element: <div>About</div>,
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,12 +6,9 @@ import { LoginForm } from '@/components/molecules/LoginForm'
 import { Button } from '@/components/atoms/Button'
 import { useAuth } from '@/core/hooks/useAuth'
 
-const GoogleButton = styled(Button)`
-  background-color: #4285F4;
-  color: white;
-  margin-top: 1rem;
-  width: 100%;
-`
+
+
+
 
 export const LoginPage: React.FC = () => {
   const [error, setError] = useState('')
@@ -37,12 +34,74 @@ export const LoginPage: React.FC = () => {
   }
 
   return (
-    <AuthContainer>
-      <h1>Iniciar Sesión</h1>
-      <LoginForm onSubmit={handleEmailLogin} error={error} />
-      <GoogleButton onClick={handleGoogleLogin}>
-        Iniciar Sesión con Google
-      </GoogleButton>
-    </AuthContainer>
+    <>
+      <PageContainer>
+        <ImageContainer />
+        <LoginContainer>
+          <h1>Iniciar Sesión</h1>
+          <LoginForm onSubmit={handleEmailLogin} error={error} />
+          <GoogleButton onClick={handleGoogleLogin}>
+            Iniciar Sesión con Google
+          </GoogleButton>
+        </LoginContainer>
+      </PageContainer>
+      {/* <AuthContainer>
+        <h1>Iniciar Sesión</h1>
+        <LoginForm onSubmit={handleEmailLogin} error={error} />
+        <GoogleButton onClick={handleGoogleLogin}>
+          Iniciar Sesión con Google
+        </GoogleButton>
+      </AuthContainer>
+      <ImageContainer /> */}
+    </>
   )
+
 }
+
+
+
+
+const PageContainer = styled.div`
+display: flex;
+height: 100vh;
+@media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const ImageContainer = styled.div`
+flex: 0;
+background-image: url('src/assets/gradient.jpg');
+background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 30vh;
+
+  @media (min-width: 768px) {
+    flex: 2;
+    min-height: 100vh;
+  }
+`;
+
+const LoginContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 2rem;
+  background-color: white;
+
+  @media (min-width: 768px) {
+    box-shadow: -5px 0 15px rgba(0, 0, 0, 0.1);
+    overflow-y: auto;
+    flex: 1;
+  }
+`;
+
+
+const GoogleButton = styled(Button)`
+background-color: #4285F4;
+color: white;
+margin-top: 1rem;
+width: 100%;
+`
