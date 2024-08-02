6a5019005cee0ef8304150d7cd3ca13629c470f5
Jose Muñoz <jmunozlo.dev@gmail.com>
2024-08-01 21:32:07 -0400

    fix: css

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,29 +1,28 @@
 import { useAuth } from 'reactfire'
 /* import { Button } from '@/components/atoms/Button' */
 import styled from 'styled-components'
-
+import '@/components/molecules/Header.css'
 export const Header = () => {
   const auth = useAuth()
-  console.log('auth', auth)
   return (
     <ContainerNavbar>
       <Logo />
       <Navbar>
-        <Ul>
+        <ul>
           <li>
             <a href="/dashboard">Dashboard</a>
           </li>
           <li>
             <a href="/agenda">Agenda</a>
           </li>
-        </Ul>
+        </ul>
         <Button onClick={() => auth.signOut()}>Sign Out</Button>
       </Navbar>
     </ContainerNavbar>
   )
 }
 
-const ContainerNavbar = styled.div`
+const ContainerNavbar = styled.header`
 display: flex;
 justify-content: center;
 background-color:#5377fa;
@@ -60,22 +59,4 @@ align-items: center;
 background-image: url('src/assets/schedule.svg');
 height: 2rem;
 width: 2rem;
-`
-
-const Ul = styled.ul`
-  display: flex;
-  list-style: none;
-  & li {
-    color: white;
-  }
-  & li a {
-    color: white;
-    text-decoration: none;
-    padding: 0.5rem;
-    height: 3rem;
-    border-radius: 4px;
-  }
-  & li a:hover {
-    background-color: #6e8cf8;
-  }
 `
